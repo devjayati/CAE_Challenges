2b5239c42924419533fe0f8c734126fbc377c2ea
devjayati <devjayati@outlook.com>
2020-11-18 11:43:01 -0500

    EH lesson 3

--- EthicalHackingLesson3/function.js
+++ EthicalHackingLesson3/function.js
@@ -1,5 +1,24 @@
 function myFunction() {
-  var selection = document.getElementsByClassName("disclose");
-  var results = document.getElementById("results");
-		results.style.display = "block";
+	var results = document.getElementById("results");
+	results.style.display = "block";
+	var x = document.getElementsByClassName("scene")[0].id;
+	var img1;
+	if(document.getElementById("public").checked){
+		img1="public";
+	}
+	else if(document.getElementById("private").checked){
+		img1="private";
+	}
+	else if(document.getElementById("mixed").checked){
+		img1="mixed";
+	}
+	else if(document.getElementById("none").checked){
+		img1="none";
+	}
+	var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			document.getElementById("display-results").innerHTML = this.responseText;
+    };
+    xmlhttp.open("GET", "index.php?q=" + img1 + '&x=' + x, true);
+    xmlhttp.send();
 }
---
+++ EthicalHackingLesson3/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$q = $_GET['q'];
+$x = $_GET['x'];
+console.log("q="+q+"and x="+x);
+$servername = "db.sice.indiana.edu";
+$username = "phishingsummer19";
+$password = "DK-PS-Nineteen";
+$db = "phishingsummer19";
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql="SELECT * FROM test WHERE id = '".$x."' AND challenge = '".$q."'";
+$result = $conn->query($sql);
+
+/* echo "<table>
+<tr>
+<th>challengeindex</th>
+<th>answer</th>
+<th>response</th>
+</tr>"; */
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+	  //echo "<tr>";
+	  //echo "<td>" . $row['challenge'] . "</td>";
+	  //echo "<td>" . $row['id'] . "</td>";
+	  echo "<td>" . $row['flag'] . "</td>";
+	  //echo "</tr>";
+	}
+}
+else {
+	//echo $q;
+	//echo $x;
+	echo "Hm..possibly. Try a different answer?";
+}
+//echo "</table>";
+$conn->close();
+?>
+</body>
+</html>
